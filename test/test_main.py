9e77448b04f73a5f13219bacf9f22c5a45a175f2
豆瑞瑞 <douzengrui@gmail.com>
2025-04-15 14:48:33 +0800

    添加 login 和 register 接口的测试

--- test/test_main.py
+++ test/test_main.py
@@ -1,10 +1,7 @@
 import pytest
 from fastapi.testclient import TestClient
-from sqlmodel import select
 
 from main import app
-from models import User
-from utils.auth import hash_password
 from utils.database import init_db, get_session
 
 
@@ -24,10 +21,9 @@ def test_register(client, test_db):
     username = "testuser"
     password = "testpassword"
 
-    # 请求注册接口
     response = client.post(
         "/register/",
-        json={"username": username, "password": password}
+        params={"username": username, "password": password}
     )
 
     assert response.status_code == 201
@@ -35,26 +31,18 @@ def test_register(client, test_db):
     assert response.json()["id"] is not None
     assert "url" in response.json()
 
-    # 检查数据库中是否有该用户
-    user_in_db = test_db.exec(select(User).where(User.username == username)).first()
-    assert user_in_db is not None
-    assert user_in_db.username == username
 
-
-def test_register_existing_user(client, test_db):
-    # 先创建一个用户
-    username = "existinguser"
+def test_login(client, test_db):
+    username = "testuser"
     password = "testpassword"
-    hashed_password = hash_password(password)
-    new_user = User(username=username, hashed_password=hashed_password)
-    test_db.add(new_user)
-    test_db.commit()
 
-    # 尝试再次注册该用户
     response = client.post(
-        "/register/",
-        json={"username": username, "password": password}
+        "/login/",
+        params={"username": username, "password": password}
     )
 
-    assert response.status_code == 400
-    assert response.json()["detail"] == "Username already exists"
+    assert response.status_code == 200
+    assert "access_token" in response.json()
+    assert response.json()["token_type"] == "bearer"
+    assert response.json()["expires_in"] == 3600
+    assert response.json()["user"]["username"] == username
