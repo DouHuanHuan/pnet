60e2f8b6693f9a554532cc9253c690e61ec4132a
豆瑞瑞 <douzengrui@gmail.com>
2025-04-16 12:10:52 +0800

    修改 runner.py 优化启动逻辑

--- utils/runner.py
+++ utils/runner.py
@@ -1,5 +1,6 @@
 import subprocess
 import sys
+from multiprocessing import Process
 
 import uvicorn
 
@@ -7,21 +8,17 @@ import uvicorn
 def start_server():
     """
     curl -X POST "http://127.0.0.1:8000/run-pnet/" \
-    -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0dXNlciIsImV4cCI6MTc0NDcwMjQyOX0.gKr4D-yKHEIJhSqO-xnAfY8uM2kvzH7SrNuSyv2MrBI" \
+    -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0dXNlciIsImV4cCI6MTc0NDc3NjcyOX0.pC-QVs5OfLbaKv15sq7ZfuGcwWVNYV06ufErGxaN71I" \
     -F "config_file=@/home/wsl/project/pnet/data/fmri_surf_hcp10subjs.toml" \
     --max-time 600
     """
-    uvicorn.run(
-        "main:app",
-        host="127.0.0.1",
-        port=8000,
-        reload=True,
-        timeout_keep_alive=120
-    )
-
-    start_celery_worker()
+    Process(target=start_fastapi).start()
+    Process(target=start_celery).start()
 
 
-def start_celery_worker():
-    celery_command = [sys.executable, "-m", "celery", "-A", "task_queue.tasks..celery_app", "worker", "--loglevel=info"]
-    subprocess.Popen(celery_command)
+def start_fastapi():
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+
+
+def start_celery():
+    subprocess.run([sys.executable, "-m", "celery", "-A", "task_queue.tasks.celery_app", "worker", "--loglevel=info"])
